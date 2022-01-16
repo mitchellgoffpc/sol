f3f0b97fb80fd9510f056e6267404e094b7d8724
Mitchell Goff <mitchellgoffpc@gmail.com>
2022-01-15 17:16:32 -0800

    Added trees to terrain generation

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -19,6 +19,8 @@ export default class Blocks {
     static Dirt  = new Block (1, times (12, _ => '#D68653'), times (12, _ => '#E3A066'))
     static Grass = new Block (2, ['#369940', '#369940', ...times (10, _ => '#D68653')],
                                  ['#43B353', '#43B353', ...times (10, _ => '#E3A066')])
+    static Log   = new Block (3, times (12, _ => '#4f382e'), times (12, _ => '#664a3e'))
+    static Leaf   = new Block (4, times (12, _ => '#059033'), times (12, _ => '#1db54f'))
 
     static All = pickBy (Blocks, value => value instanceof Block)
     static ByID = fromPairs (map (Blocks.All, block => [block.ID, block]))
--- src/world/terrain.worker.js
+++ src/world/terrain.worker.js
@@ -8,6 +8,7 @@ import { getBlockIndex } from 'util/coordinates'
 // Constants
 
 const noise = new SimplexNoise ('seed')
+const treeNoise = new SimplexNoise('trees')
 
 
 // Attach the message listener
@@ -43,20 +44,48 @@ function createChunkTerrainForBatch ({ positions }) {
 
 // Helper functions
 
+const getElevation = (x, z, bx, bz) =>
+    Math.floor ((noise.noise2D ((x * 16 + bx) / 64,  (z * 16 + bz) / 64) / 2 + .5) * 8) +
+    Math.floor ((noise.noise2D ((x * 16 + bx) / 256, (z * 16 + bz) / 256) / 2 + .5) * 64)
+
+const hasTree = (x, z, bx, bz) =>
+    treeNoise.noise2D (x * 16 + bx, z * 16 + bz) > 0.98
+
+function addBlock (blocks, x, y, z, blockId) {
+    if (x >= 0 && x < 16 && z >= 0 && z < 16 && y >= 0 && y < 16) {
+        blocks[getBlockIndex (x, y, z)] = blockId }}
+
+function addTree (blocks, x, y, z) {
+    for (let dx = -3; dx <= 3; dx++) {
+        for (let dz = -3; dz <= 3; dz++) {
+            for (let dy = 0; dy <= 3; dy++) {
+                if (dx*dx + dy*dy + dz*dz < 9) {
+                    addBlock (blocks, x + dx, y + dy + 4, z + dz, Blocks.Leaf.ID) }}}}
+
+    for (let dy = 0; dy < 5; dy++) {
+        addBlock (blocks, x, y + dy, z, Blocks.Log.ID) }}
+
+
+
 function createChunkTerrain (x, y, z) {
     const blocks = new Int32Array (16 * 16 * 16)
     let chunkContainsBlocks = false
 
     for (let bx = 0; bx < 16; bx++) {
         for (let bz = 0; bz < 16; bz++) {
-            const elevation =
-                Math.floor ((noise.noise2D ((x * 16 + bx) / 64,  (z * 16 + bz) / 64) / 2 + .5) * 8) +
-                Math.floor ((noise.noise2D ((x * 16 + bx) / 256, (z * 16 + bz) / 256) / 2 + .5) * 64) -
-                y * 16
+            const elevation = getElevation (x, z, bx, bz) - y * 16
 
             for (let by = 0; by <= elevation && by < 16; by++) {
                 blocks[getBlockIndex (bx, by, bz)] = by >= elevation - 1 ? Blocks.Grass.ID : Blocks.Dirt.ID
                 chunkContainsBlocks = true }}}
+
+    for (let bx = -3; bx < 19; bx++) {
+        for (let bz = -3; bz < 19; bz++) {
+            const elevation = getElevation (x, z, bx, bz) - y * 16
+
+            if (elevation >= -8 && elevation < 15 && hasTree (x, z, bx, bz)) {
+                addTree (blocks, bx, elevation + 1, bz)
+               chunkContainsBlocks = true }}}
 
     return chunkContainsBlocks ? blocks : null }
 
